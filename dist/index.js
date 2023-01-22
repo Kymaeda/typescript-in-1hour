88fcbf04f49a9cc95c52de53009baed857f8a3e5
Kymaeda <kyo.m.720@gmail.com>
2023-01-22 09:16:20 +0900

    debugging

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
 "use strict";
 console.log('OK');
 let age = 11;
+if (age > 20)
+    'adult';
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 console.log('OK')
 let age: number = 11;
-
+if (age > 20)
+  'adult'
